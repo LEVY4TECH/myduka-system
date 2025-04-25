5c80762b186c86fc1a09350ec5921b33017d89b5
LEVY4TECH <levy4tech@gmail.com>
2025-04-25 10:34:03 -0700

    4/25

---
+++ main.py
@@ -0,0 +1,25 @@
+# importinf flask to use it
+from flask import Flask, render_template
+
+# instantiate your application - initialization 
+# flask instance
+app= Flask(__name__)
+
+# __name__ - informs pyhton that this is the main application
+# __name__ - special inbuilt variable 
+# represents the name of the current file where your application is built 
+# informs flask where your project starts
+
+@app.route('/')
+def home():
+    return render_template ('index.html')
+
+@app.route('/products')
+def products():
+    return render_template ('products.html')
+
+@app.route('/categories')
+def categories():
+    return render_template ('categories.html')
+
+app.run(debug=True)
